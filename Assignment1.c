90b3da90b642c7b486da5fc8eaf63511032ba580
adityaraj3324 <asahu3324@gmail.com>
2025-03-31 22:38:47 +0530

    C-Logical

---
+++ Assignment1.c
@@ -0,0 +1,52 @@
+//Q1. WAP to print five ....
+
+
+#include <stdio.h>
+
+int main()
+{   
+    //five friends print
+   printf("Aditya \n Aman \n Anil \n Nilesh \n Arin\n");
+    
+    //five country namespace
+    printf("Algeria \n Afganistan \n Alaska \n Nigeria \n Argentina\n");
+    
+    //five fav city name
+     printf("jaisalmer \n jaipur \n Delhi \n Ayodhya \n udaipur\n");
+     
+     //five fav food name
+      printf("rajma chawal \n aloo pyaaz sabji \n paneer \n burger \n Paav bhaji\n");
+      
+      //five fav games
+       printf("chess \n badminton \n basketball \n volleyball \n cricket\n");
+    return 0;
+}
+
+-----------------------------------------------------------------------------------------------------------------
+//Q2. WAP to print int and float values.
+
+
+
+#include <stdio.h>
+
+int main()
+{   
+   int a =  783 , b= 12,c = -34; float  d= 7.8, e= 123.4223;
+   printf("%d\n%d\n%f\n%d\n%f", a, c , d, b, e);
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------
+//Q3. WAP to print char 
+
+
+#include <stdio.h>
+
+int main()
+{   
+   char a = 'r' , b= 'd',c = 'k',  d= 'w', e= 'o',f= 'p';
+   printf("%c\n%c\n%c\n%c\n%c\n%c", a, c , d, b, e,f);
+    return 0;
+}
+
+------------------------------------------------------------------------------------------------------------------
